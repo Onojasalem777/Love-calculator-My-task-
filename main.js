03e9971eb3f68bf96824c81c7c2217d0e59108fe
Dev Salem <89421260+Onojasalem777@users.noreply.github.com>
2022-11-04 15:56:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+// var myName = "Salem";
+// var yourName = prompt("what is your name?");
+// alert("my name is " + myName + ",welcome to my world " + yourName + "!" );
+
+// var gameLevel = 1;
+
+
+
+
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+
+
+//**************************************** love calculator *************************************************
+
+// prompt("What is your?");
+// prompt("What is their name?");
+
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor (loveScore) + 1;
+// alert("Your love score is " + loveScore + "%");
+
+
+
+// *************************  Using if statement  ***************************
+
+// prompt("What is your?");
+// prompt("What is their name?");
+
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor (loveScore) + 1; /*1 - 100*/
+
+
+
+// if (loveScore > 70) {
+//   alert("Your love score is " + loveScore + "%" + " Oga na manage she wan manage u so");
+// } 
+
+
+
+// else {
+//      alert ("your love score na " + loveScore +  "%" + " She cant love you Oga go rest e")
+// }
+
+
+
+
+//*********************** The complete calculator 1 ********************* */
+
+// prompt("What is your?");
+// prompt("What is their name?");
+
+
+// var loveScore = Math.random() * 100;
+// loveScore = Math.floor (loveScore) + 1; /*1 - 100*/
+
+
+
+// if (loveScore > 70) {
+//   alert("Your love score is " + loveScore + "%" + " Oga na manage she wan manage u so");
+// } 
+
+
+// if (loveScore > 30 && loveScore <= 70) {
+//     alert ("your love score na " + loveScore +  "%" + " She cant love you Oga go rest e");
+// }
+
+
+// if ( loveScore <= 30) {
+//      alert ("your love score na " + loveScore +  "%" + " d luv no go even work yus forget");
+// }
+
+
+
+
+/***************************** The Complete Caculator 2 *******************************/ 
+
+prompt("What is your?");
+prompt("What is their name?"); 
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor (loveScore) + 1; /*1 - 100*/
+
+
+
+if (loveScore > 70) {
+  alert("Your love score is " + loveScore + "%" + " Oga na manage she wan manage u so");
+} 
+
+
+ if ( loveScore > 30 && loveScore <= 70) {
+     alert ("your love score na " + loveScore +  "%" + " She cant love you Oga go rest e");
+    
+}
+
+if (loveScore <= 30) {
+    alert ("your love score na " + loveScore +  "%" + " sorry oh sorry oh, she no love you");    
+}
